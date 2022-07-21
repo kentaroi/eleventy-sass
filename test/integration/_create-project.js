578b0ffd25dac96485c1b4ab134016937828a8b2
Kentaro Imai <kentaroi@gmail.com>
2022-07-22 01:18:27 +0900

    Fix createProject()

--- test/integration/_create-project.js
+++ test/integration/_create-project.js
@@ -6,14 +6,10 @@ module.exports = function(projectName) {
   let projectDir = path.join(__dirname, "fixtures", projectName);
   fs.rmSync(projectDir, { recursive: true, force: true });
   fs.mkdirSync(projectDir);
-  fs.mkdirSync(path.join(projectDir, "dist"));
 
   let names = fs.readdirSync(sourceDir);
   names.forEach(name => {
     fs.symlinkSync(path.join(sourceDir, name), path.join(projectDir, name));
-  });
-  ["package.json", "package-lock.json", "node_modules"].forEach(name => {
-    fs.symlinkSync(name, path.join(projectDir, name));
   });
 
   return projectDir;
