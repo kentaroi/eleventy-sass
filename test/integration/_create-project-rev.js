f16e835d9b439e314fb256f89d626f5ebf197379
Kentaro Imai <kentaroi@gmail.com>
2024-08-14 03:22:40 +0900

    Stop using symbolic links in tests
    Symbolic links might not work in some environments in Windows.

--- test/integration/_create-project-rev.js
+++ test/integration/_create-project-rev.js
@@ -5,12 +5,7 @@ const sourceDir = path.join(__dirname, "fixtures", "eleventy-project-rev");
 module.exports = function(projectName) {
   let projectDir = path.join(__dirname, "fixtures", projectName);
   fs.rmSync(projectDir, { recursive: true, force: true });
-  fs.mkdirSync(projectDir);
-
-  let names = fs.readdirSync(sourceDir);
-  names.forEach(name => {
-    fs.symlinkSync(path.join(sourceDir, name), path.join(projectDir, name));
-  });
+  fs.cpSync(sourceDir, projectDir, { recursive: true });
 
   return projectDir;
 };
